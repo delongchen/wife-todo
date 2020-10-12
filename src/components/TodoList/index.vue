7e81abe2ae338005333270f8560ebe66b663a194
delongchen <fg456@vip.qq.com>
2020-10-12 18:54:27 +0800

    footle

--- src/components/TodoList/index.vue
+++ src/components/TodoList/index.vue
@@ -1,26 +1,10 @@
 <template>
-  <todo-cell :info="DATA"/>
+  <div>?</div>
 </template>
 
 <script>
-import TodoCell from "@/components/TodoList/TodoCell";
-import { DATA } from "@/components/TodoList/data";
-
 export default {
-  name: "TodoList",
-  components: {TodoCell},
-  data() {
-    return {
-      DATA
-    }
-  },
-  created() {
-    fetch('todos.json').then(value => {
-      value.text().then(json => {
-        console.log(json)
-      })
-    })
-  }
+  name: "index"
 }
 </script>
 
